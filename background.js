cb060d1c12d1a788680d7fa6f5d350e4f22efdd2
Shiny <contact@shinychang.net>
2019-10-15 23:29:56 +0800

    Apply error handling

--- background.js
+++ background.js
@@ -6,7 +6,9 @@ chrome.webRequest.onSendHeaders.addListener(
     if (!tabs[tabId]) {
       tabs[tabId] = new Set();
     }
-    tabs[tabId].add(new URL(url).host);
+    try {
+      tabs[tabId].add(new URL(url).host);
+    } catch (e) {}
     chrome.browserAction.setBadgeText({ text: tabs[tabId].size.toString(), tabId });
   },
   { urls: ['<all_urls>'] }
@@ -17,7 +19,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (t.length === 0) {
       return;
     }
-    sendResponse(Array.from(tabs[t[0].id]));
+    sendResponse(Array.from(tabs[t[0].id] || []));
   });
   return true;
 });
